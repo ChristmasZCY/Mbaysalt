44ece7b5c26dacd7e0d8f8fb92283924b2e5080c
Christmas <273519355@QQ.COM>
2024-03-21 20:03:55 +0800

    Added Otherpkgs

---
+++ Exfunctions/Otherpkgs/Otherpkgs.m
@@ -0,0 +1,5 @@
+function Otherpkgs
+    % <a href="matlab: web('https://ww2.mathworks.cn/matlabcentral/fileexchange/24497-rgb-triple-of-color-name-version-2')">RGB triple of color name</a>.
+    % <a href="matlab: web('https://ww2.mathworks.cn/matlabcentral/fileexchange/85900-seawater-density-from-salinity-temperature-and-pressure')">Seawater density from salinity, temperature and pressure</a>.
+    help Otherpkgs
+end
