2dedc770cc214ac765ae29026a85a9c0e218d236
Christmas <273519355@QQ.COM>
2024-05-18 01:33:30 +0800

    Fixed

--- ST_Mbaysalt.m
+++ ST_Mbaysalt.m
@@ -553,7 +553,7 @@ function CLONE = git_clone(Git, pkg_url, pkg_path, Depth, Branch)
         disp(txt)
         system(txt);
     case {'matlab'}
-        CLONE = gitclone(pkg_url, pkg_path, 'Depth', only_new);
+        CLONE = gitclone(pkg_url, pkg_path, 'Depth', str2double(Depth));
     end
 end
 
