1402bd57bd75fdb1c51b07125ec01e13ad30a945
Christmas <273519355@qq.com>
2024-07-26 11:21:57 +0800

    Release 2.4

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Mbaysalt Toolbox
-% Version 2.3 (R2024a) 2024-04-04
+% Version 2.4 (R2024a) 2024-07-26
 % =================================================================================================================
 % Version 0.1  2023-03-01   R2023b  (Created by Christmas, Dovelet, Qidi Ma)
 % Version 1.0  2023-10-18   R2023b  (Fixed by Christmas)
@@ -7,6 +7,7 @@
 % Version 2.1 (R2023b) 2023-12-27   (Modified by Christmas)
 % Version 2.2 (R2023b) 2024-01-31   (Modified by Christmas) --->  Release: <https://github.com/ChristmasZCY/Mbaysalt/releases/tag/release-v2.2>
 % Version 2.3 (R2024a) 2024-04-04   (Modified by Christmas) --->  Release: <https://github.com/ChristmasZCY/Mbaysalt/releases/tag/release-v2.3>
+% Version 2.4 (R2024a) 2024-07-26   (Modified by Christmas) --->  Release: <https://github.com/ChristmasZCY/Mbaysalt/releases/tag/release-v2.4>
 %
 % This toolbox is used to postpocess model data and draw pictures.
 % The model data is from FVCOM, NEMURO, ERSEM, WAVE WATCH III, WRF, MITgcm and so on.
