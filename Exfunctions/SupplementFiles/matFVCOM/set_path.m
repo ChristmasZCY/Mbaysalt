811b119188cec5717c894064587a86e5dcaf59ad
Christmas <273519355@qq.com>
2024-09-12 18:31:59 +0800

    Fixed matFVCOM

---
+++ Exfunctions/SupplementFiles/matFVCOM/set_path.m
@@ -0,0 +1,35 @@
+%==========================================================================
+% Set the path for the matFVCOM package
+%
+% input  :
+%   \
+% output :
+%   PATH
+%
+% Siqi Li, SMAST
+% 2022-12-16
+%
+% Updates:
+%
+%==========================================================================
+function PATH = set_path
+
+% Type your GSHHS coastline data directory here
+PATH.gshhs = '/Users/christmas/Documents/Code/Project/Server_Program/Mbaysalt/Exfunctions/m_map/data';
+% Type your ETOPO1 data path here
+PATH.etopo1 = 'C:\data\ETOPO1_Bed_g_gmt4.grd';
+
+if isempty(PATH.gshhs)
+    
+        PATH.gshhs = [fundir('set_path') 'data'];
+end
+if isempty(PATH.etopo1)
+    if contains(computer, 'WIN')
+        PATH.etopo1 = [fundir('set_path') 'data\ETOPO1_Bed_g_gmt4.grd'];
+    else
+        PATH.etopo1 = [fundir('set_path') 'data/ETOPO1_Bed_g_gmt4.grd'];
+    end
+end
+
+
+end
