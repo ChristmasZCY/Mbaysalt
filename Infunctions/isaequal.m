05d15a7f1530fa35c5309c78e356119a297f9c94
Christmas <273519355@qq.com>
2026-04-23 14:47:08 +0800

    Fixed isaequal error at R2022b

--- Infunctions/isaequal.m
+++ Infunctions/isaequal.m
@@ -1,17 +1,18 @@
-function TF = isaequal(A, B, eps, varargin)
+function TF = isaequal(A, B, tol, varargin)
     %       approximate equality A, B(not support NaN)
     % =================================================================================================================
     % Parameters:
     %       A:          input A             || required: True       || type: matrix     || format: matrix
     %       B:          input B             || required: True       || type: matrix     || format: matrix
-    %       eps:        tolerance           || required: positional || type: double     || format: numeric
+    %       tol:        tolerance           || required: positional || type: double     || format: numeric
     %       varargin: (optional) 
     % =================================================================================================================
     % Returns:
     %       TF:         logical             || required: True       || type: logical    || format: matrix
     % =================================================================================================================
     % Updates:
-    %       2024-09-19:     Created,    by Christmas;
+    %       2024-09-19:     Created,                by Christmas;
+    %       2026-04-23;     Fixed error at R2022b,  by Christmas;
     % =================================================================================================================
     % Examples:
     %       TF = isaequal(A, B);
@@ -25,18 +26,17 @@ function TF = isaequal(A, B, eps, varargin)
         error('There''s NaN in %s or %s, please use ''isaequaln'' instead!', inputname(1), inputname(2))
     end
 
-    % eps也是函数
-
+    % eps是函数
+    if ~ exist("tol","var") == 1
+        tol = eps;
+    end
 
     if ~isMATLABReleaseOlderThan("R2024b")
-        if exist("eps","var")
-            TF1 = isapprox(A, B, "AbsoluteTolerance", eps);
-        else
-            TF1 = isapprox(A, B);
-        end
+        % TF1 = isapprox(A, B);
+        TF1 = isapprox(A, B, "AbsoluteTolerance", tol);
         TF = all(TF1,'all');
     else
-        TF = max(A-B)<eps;
+        TF = max(A-B)<tol;
     end
     
     return
