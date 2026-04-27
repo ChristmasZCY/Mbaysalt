c971260533a6d7e90f3d52bd0a5149090be0ce78
Christmas <273519355@qq.com>
2026-04-27 23:33:16 +0800

    Fixed error: onCleanup at Ncfunctions.

--- Ncfunctions/+netcdf_fvcom/wrnc_adt.m
+++ Ncfunctions/+netcdf_fvcom/wrnc_adt.m
@@ -228,7 +228,7 @@ function rtn = wrnc_adt(NC, Lon, Lat, time, Zeta, varargin)
             netcdf.putAtt(ncid, varid_GA, 'Mbaysalt_version', ver('Mbaysalt').Version); % Mbaysalt版本信息
             netcdf.putAtt(ncid, varid_GA, 'Mbaysalt_gitHash', getGitHash(ST_Mbaysalt('cd'), 'long')); % Mbaysalt git hash
             netcdf.putAtt(ncid, varid_GA, 'MATLAB_version', version); % MATLAB版本信息
-            netcdf.close(ncid); % 关闭nc文件
+            % netcdf.close(ncid);  % ncid is closed automatically by cleanupObj. % 关闭nc文件
 
         case 'HighLevel'
             ncname = getPath(ncname);
